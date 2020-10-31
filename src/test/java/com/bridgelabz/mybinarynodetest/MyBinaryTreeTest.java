9c582b776f0f81c1f1dc984cc39f224782a60953
Raj <rajkamal11126@gmail.com>
2020-10-31 22:26:01 +0530

    Added : Size method in binary search tree

---
+++ src/test/java/com/bridgelabz/mybinarynodetest/MyBinaryTreeTest.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.mybinarynodetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.mybinarynode.MyBinaryTree;
+
+public class MyBinaryTreeTest {
+	
+	/**
+	 * Added 13 numbers in binary tree and return size
+	 */
+	@Test
+	public void given13NumbersToAdd_WhenAnlysed_ShouldReturnSize13() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		Assert.assertEquals(13, myBinaryTree.size());
+	}
+
+}

c54cc1f225aa13f4a0c45a34d8587bcdf879c157
Raj <rajkamal11126@gmail.com>
2020-10-31 21:23:17 +0530

    Added : three element added in binary search tree

---
+++ src/test/java/com/bridgelabz/mybinarynodetest/MyBinaryNodeTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.mybinarynodetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.mybinarynode.MyBinaryTree;
+
+public class MyBinaryNodeTest {
+	/**
+	 * Added three numbers in binary search tree 
+	 */
+	@Test
+	public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSizeThree() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		Assert.assertEquals(3, myBinaryTree.size());
+	}
+}
